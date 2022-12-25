014903fbedb6931010e872e1bef43b535b0892ea
AntonZyuzin <71172648+AntonZyuzin@users.noreply.github.com>
2022-12-25 21:33:08 +0300

    Add files via upload

---
+++ DiscordBotMarinaV2/DiscordBot.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBotMarinaV2
+{
+    public class DiscordBot
+    {
+        private DiscordSocketClient _client;
+        private readonly string _token;
+        private readonly string _unwantedRole;
+        private readonly List<string> _allowedMessagesList;
+        private readonly string _allowedChannel;
+
+        public DiscordBot(
+            string token, 
+            string role, 
+            List<string> allowedMessagesList, 
+            string allowedChannel
+            )
+        {
+            _token = token;
+            _unwantedRole = role;
+            _allowedMessagesList = allowedMessagesList;
+            _allowedChannel = allowedChannel;
+        }
+
+        public async Task WorkAsync()
+        {
+
+            var socketConfig = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+
+            _client = new DiscordSocketClient(socketConfig);
+            _client.MessageReceived += DoModeration;
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+
+        private Task Log(LogMessage logMsg)
+        {
+            Console.WriteLine(logMsg);
+            return Task.CompletedTask;
+        }
+
+        private Task DoModeration(SocketMessage message)
+        {
+            if (!message.Author.IsBot)
+            {
+                if (_allowedChannel == message.Channel.ToString())
+                {
+                    var authorRolesList = ((SocketGuildUser)message.Author)
+                        .Roles
+                        .Select(authorRole => authorRole.Name)
+                        .ToList();
+
+                    if (authorRolesList.Contains(_unwantedRole))
+                    {
+                        if (!_allowedMessagesList.Contains(message.Content.ToLower()))
+                        {
+                            message.Channel.DeleteMessageAsync(message);
+                            Console.WriteLine("Я удалила сообщение пользователя "
+                                + message.Author.ToString()
+                                + ", потому что он петух (⌒‿⌒)");
+                        }
+                    }
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DiscordBotMarinaV2/Program.cs
@@ -0,0 +1,62 @@
+﻿using DiscordBotMarinaV2;
+using System.Configuration;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+            var token = ConfigurationManager.AppSettings.Get("token");
+            var role = ConfigurationManager.AppSettings.Get("role");
+            var commandsPrefix = ConfigurationManager.AppSettings.Get("commandsPrefix");
+            List<string> allowedMessagesList = ConfigurationManager.AppSettings
+                                                          .Get("allowedMessages")
+                                                          .ToLower()
+                                                          .Split(' ')
+                                                          .Select(s => $"{commandsPrefix + s}")
+                                                          .ToList();
+            var allowedChannel = ConfigurationManager.AppSettings.Get("allowedChannel");
+
+
+
+
+        PrintCfg(token, role, commandsPrefix, allowedMessagesList, allowedChannel);
+
+        var bot = new DiscordBot(token, role, allowedMessagesList, allowedChannel);
+
+        while(true)
+        {
+            Console.WriteLine("Starting...");
+            bot.WorkAsync().GetAwaiter().GetResult();
+        }
+    }
+
+    static void PrintCfg(
+        string token, 
+        string role, 
+        string commandsPrefix, 
+        List<string> allowedMessagesList, 
+        string allowedChannel
+        )
+    {
+        Console.WriteLine("Token:");
+        Console.WriteLine(token);
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Role:");
+        Console.WriteLine(role);
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Commands prefix:");
+        Console.WriteLine(commandsPrefix);
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Allowed messages:");
+        foreach (string m in allowedMessagesList)
+        {
+            Console.WriteLine(m);
+        }
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Allowed channel:");
+        Console.WriteLine(allowedChannel);
+        Console.WriteLine("----------------------------------------");
+
+    }
+}
